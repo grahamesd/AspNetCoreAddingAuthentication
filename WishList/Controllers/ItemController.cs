57ffa75a42e11caac462d7d8f053bb9de1a7eb28
Grahame Scott-Douglas <grahame@scott-douglas.net>
2018-09-25 16:57:48 -0700

    Took into account that ApplicationUser imlements the IEquatable interface

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -49,7 +49,7 @@ namespace WishList.Controllers
         {
             var user = _userManager.GetUserAsync(User).Result;
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
-            if (item.User.Id != user.Id)
+            if (item.User != user)
             {
                 return Unauthorized();
             }
